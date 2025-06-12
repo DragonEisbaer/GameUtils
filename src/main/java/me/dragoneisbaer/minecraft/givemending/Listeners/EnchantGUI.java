b0dfac6e234f12a550404a60eb5358aa8fa120bf
Brian <dragoneisbaer07@gmail.com>
2025-06-12 16:16:14 +0200

    todo: logic to give item back in lvl selector & better ui for sideswap

--- src/main/java/me/dragoneisbaer/minecraft/givemending/Listeners/EnchantGUI.java
+++ src/main/java/me/dragoneisbaer/minecraft/givemending/Listeners/EnchantGUI.java
@@ -12,6 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,7 +23,7 @@ public class EnchantGUI implements Listener {
     GameUtils plugin = JavaPlugin.getPlugin(GameUtils.class);
 
     HashMap<Player, Integer> page = new HashMap<>();
-    ArrayList<ItemStack> alloweditems = new ArrayList<>();
+    private final Set<Material> alloweditems = new HashSet<>();
     private boolean openingNewInventory = false;
 
     private Inventory inv;
@@ -36,8 +37,8 @@ public class EnchantGUI implements Listener {
             return;
         }
 
-        setAllowedItems();
-        if (!alloweditems.contains(event.getCurrentItem())) {
+        setAllowedMaterials();
+        if (!alloweditems.contains(event.getCurrentItem().getType())) {
             event.setCancelled(true);
             return;
         }
@@ -97,27 +98,23 @@ public class EnchantGUI implements Listener {
             return;
         }
         Player player = (Player) e.getWhoClicked();
+        int currentPage = page.getOrDefault(player, 1);
+        int maxPage = (int) Math.ceil((double) getAllPossibleEnchantments(player).size() / 15.0);
 
-        if (page.containsKey(player)) {
-            switch (page.get(player)) {
-                case 1:
-                    setInv(player, 15);
-                    page.put(player, 2);
-                    break;
-                case 2:
-                    setInv(player, 30);
-                    page.put(player, 3);
-                    break;
-                case 3:
-                    setInv(player, 45);
-                    page.put(player, 4);
-                    break;
-            }
-            plugin.getLogger().info("Set Page: " + page.get(player) + " for " + player.getName());
+        if (e.getClick() == ClickType.LEFT && currentPage < maxPage) {
+            setInv(player, currentPage * 15);
+            page.put(player, currentPage + 1);
+        } else if (e.getClick() == ClickType.RIGHT && currentPage > 1) {
+            setInv(player, (currentPage - 2) * 15);
+            page.put(player, currentPage - 1);
+        } else {
+            e.setCancelled(true);
+            return;
         }
+        plugin.getLogger().info("Set Page: " + page.get(player) + " for " + player.getName());
         openingNewInventory = true;
-
         player.openInventory(inv);
+        e.setCancelled(true);
     }
 
     @EventHandler
@@ -165,6 +162,7 @@ public class EnchantGUI implements Listener {
         if (!e.getView().getTitle().equalsIgnoreCase(ChatColor.DARK_BLUE + "Enchant Level Selector")) {
             return;
         }
+        e.setCancelled(true);
         if (e.getCurrentItem() == null || e.getCurrentItem().getType() != Material.ENCHANTED_BOOK) {
             return;
         }
@@ -175,19 +173,49 @@ public class EnchantGUI implements Listener {
         Player player = (Player) e.getWhoClicked();
         ItemStack selectedBook = e.getCurrentItem();
 
+        if (!(selectedBook.getItemMeta() instanceof EnchantmentStorageMeta)) {
+            player.sendMessage(ChatColor.RED + "Du hast kein Buch ausgewählt, welches du verzaubern möchtest!");
+            e.setCancelled(true);
+            return;
+        }
+        EnchantmentStorageMeta enchantmentStorageMeta = (EnchantmentStorageMeta) selectedBook.getItemMeta();
+
+        if (enchantmentStorageMeta.getStoredEnchants().isEmpty()) {
+            player.sendMessage(ChatColor.RED + "Du hast kein Buch ausgewählt, welches du verzaubern möchtest!");
+            e.setCancelled(true);
+            return;
+        }
+        Enchantment bookench = enchantmentStorageMeta.getStoredEnchants().keySet().iterator().next();
+        int bookenchlevel = enchantmentStorageMeta.getStoredEnchantLevel(bookench);
+
         if (plugin.getStoredEnchantItem().containsKey(player)) {
-            ItemStack itemToEnchant = plugin.getStoredEnchantItem().get(player);
-            itemToEnchant.addUnsafeEnchantment(selectedBook.getItemMeta().getEnchants().keySet().iterator().next(), selectedBook.getEnchantmentLevel(selectedBook.getItemMeta().getEnchants().keySet().iterator().next()));
+
+            ItemStack itemToEnchant;
+
+            if (plugin.getStoredEnchantItem().get(player).getType() == Material.BOOK) {
+                itemToEnchant = new ItemStack(Material.ENCHANTED_BOOK);
+            }else {
+                itemToEnchant = plugin.getStoredEnchantItem().get(player);
+            }
+
+            if (itemToEnchant.getType() == Material.ENCHANTED_BOOK) {
+                ItemMeta meta = itemToEnchant.getItemMeta();
+                if (meta instanceof EnchantmentStorageMeta) {
+                    EnchantmentStorageMeta storageMeta = (EnchantmentStorageMeta) meta;
+                    storageMeta.addStoredEnchant(bookench, bookenchlevel, true);
+                    itemToEnchant.setItemMeta(storageMeta);
+                }
+            }else {
+                itemToEnchant.addUnsafeEnchantment(bookench, bookenchlevel);
+            }
             plugin.getStoredEnchantItem().put(player, itemToEnchant);
-            player.sendMessage(ChatColor.GREEN + "Verzauberung hinzugefügt: " + selectedBook.getItemMeta().getEnchants().keySet().iterator().next());
             openingNewInventory = true;
+            page.put(player, 1);
             setInv(player, 0);
             player.openInventory(inv);
         } else {
             player.sendMessage(ChatColor.RED + "Du hast kein Item ausgewählt, welches du verzaubern möchtest!");
         }
-
-        e.setCancelled(true);
     }
 
     private void setInv(Player player, int startIndex) {
@@ -218,11 +246,24 @@ public class EnchantGUI implements Listener {
         }
         if (safeslots.contains(27)) {
             page.putIfAbsent(player, 1);
-            if (page.get(player) != 99) {
+            int currentPage = page.getOrDefault(player, 1);
+            int maxPage = (int) Math.ceil((double) allPossibleEnchantments.size() / 15.0);
+
+            if (maxPage > 1) { // Nur anzeigen, wenn Blättern möglich ist
                 ItemStack next = new ItemStack(Material.ARROW);
                 ItemMeta meta = next.getItemMeta();
                 assert meta != null;
-                meta.setDisplayName(ChatColor.GREEN + "Next");
+                meta.setDisplayName(ChatColor.GREEN + "Seitenwechsel");
+                List<String> lore = new ArrayList<>();
+                if (currentPage == 1) {
+                    lore.add(ChatColor.DARK_GREEN + "Linksklick: Nächste Seite");
+                } else if (currentPage == maxPage) {
+                    lore.add(ChatColor.DARK_RED + "Rechtsklick: Vorherige Seite");
+                } else {
+                    lore.add(ChatColor.DARK_GREEN + "Linksklick: Nächste Seite");
+                    lore.add(ChatColor.DARK_RED + "Rechtsklick: Vorherige Seite");
+                }
+                meta.setLore(lore);
                 next.setItemMeta(meta);
                 inv.setItem(26, next);
             }
@@ -232,75 +273,41 @@ public class EnchantGUI implements Listener {
     }
 
     private void openEnchLevelSelector(Player player, ItemStack selectedBook) {
-
         Inventory enchLevelInv = getBasicInv(player, "Enchant Level Selector");
-
 
         if (selectedBook.getItemMeta().getEnchants().size() == 1) {
             Enchantment enchantment = selectedBook.getItemMeta().getEnchants().keySet().iterator().next();
+            int maxLevel = enchantment.getMaxLevel();
 
-            ItemStack level1 = new ItemStack(Material.ENCHANTED_BOOK);
-            ItemStack level2 = new ItemStack(Material.ENCHANTED_BOOK);
-            ItemStack level3 = new ItemStack(Material.ENCHANTED_BOOK);
-            ItemStack level4 = new ItemStack(Material.ENCHANTED_BOOK);
-            ItemStack level5 = new ItemStack(Material.ENCHANTED_BOOK);
-
-            switch (enchantment.getMaxLevel()) {
+            // Slot-Positionen je nach MaxLevel
+            int[] slots;
+            switch (maxLevel) {
                 case 1:
-                    level1.addUnsafeEnchantment(enchantment, 1);
-                    enchLevelInv.setItem(15, level1);
-                    level1.removeEnchantment(enchantment);
+                    slots = new int[]{15};
                     break;
                 case 2:
-                    level1.addUnsafeEnchantment(enchantment, 1);
-                    level2.addUnsafeEnchantment(enchantment, 2);
-                    enchLevelInv.setItem(14, level1);
-                    enchLevelInv.setItem(16, level2);
-                    level1.removeEnchantment(enchantment);
-                    level2.removeEnchantment(enchantment);
+                    slots = new int[]{14, 16};
                     break;
                 case 3:
-                    level1.addUnsafeEnchantment(enchantment, 1);
-                    level2.addUnsafeEnchantment(enchantment, 2);
-                    level3.addUnsafeEnchantment(enchantment, 3);
-                    enchLevelInv.setItem(14, level1);
-                    enchLevelInv.setItem(15, level2);
-                    enchLevelInv.setItem(16, level3);
-                    level1.removeEnchantment(enchantment);
-                    level2.removeEnchantment(enchantment);
-                    level3.removeEnchantment(enchantment);
+                    slots = new int[]{14, 15, 16};
                     break;
                 case 4:
-                    level1.addUnsafeEnchantment(enchantment, 1);
-                    level2.addUnsafeEnchantment(enchantment, 2);
-                    level3.addUnsafeEnchantment(enchantment, 3);
-                    level4.addUnsafeEnchantment(enchantment, 4);
-                    enchLevelInv.setItem(13, level1);
-                    enchLevelInv.setItem(14, level2);
-                    enchLevelInv.setItem(16, level3);
-                    enchLevelInv.setItem(17, level4);
-                    level1.removeEnchantment(enchantment);
-                    level2.removeEnchantment(enchantment);
-                    level3.removeEnchantment(enchantment);
-                    level4.removeEnchantment(enchantment);
+                    slots = new int[]{13, 14, 16, 17};
                     break;
                 case 5:
-                    level1.addUnsafeEnchantment(enchantment, 1);
-                    level2.addUnsafeEnchantment(enchantment, 2);
-                    level3.addUnsafeEnchantment(enchantment, 3);
-                    level4.addUnsafeEnchantment(enchantment, 4);
-                    level5.addUnsafeEnchantment(enchantment, 5);
-                    enchLevelInv.setItem(13, level1);
-                    enchLevelInv.setItem(14, level2);
-                    enchLevelInv.setItem(15, level3);
-                    enchLevelInv.setItem(16, level4);
-                    enchLevelInv.setItem(17, level5);
-                    level1.removeEnchantment(enchantment);
-                    level2.removeEnchantment(enchantment);
-                    level3.removeEnchantment(enchantment);
-                    level4.removeEnchantment(enchantment);
-                    level5.removeEnchantment(enchantment);
+                    slots = new int[]{13, 14, 15, 16, 17};
                     break;
+                default:
+                    slots = new int[]{13, 14, 15, 16, 17};
+                    break;
+            }
+
+            for (int level = 1; level <= maxLevel; level++) {
+                ItemStack levelBook = new ItemStack(Material.ENCHANTED_BOOK);
+                EnchantmentStorageMeta meta = (EnchantmentStorageMeta) levelBook.getItemMeta();
+                meta.addStoredEnchant(enchantment, level, true);
+                levelBook.setItemMeta(meta);
+                enchLevelInv.setItem(slots[level - 1], levelBook);
             }
         }
         player.openInventory(enchLevelInv);
@@ -328,7 +335,7 @@ public class EnchantGUI implements Listener {
     private ArrayList<Enchantment> getAllPossibleEnchantments(Player player) {
         //Check all Possible Enchants
         ArrayList<Enchantment> allPossibleEnchantments = new ArrayList<>();
-        if (plugin.getStoredEnchantItem().get(player).getType() == Material.BOOK) {
+        if (plugin.getStoredEnchantItem().get(player).getType() == Material.BOOK || plugin.getStoredEnchantItem().get(player).getType() == Material.ENCHANTED_BOOK) {
             allPossibleEnchantments.addAll(List.of(Enchantment.values()));
         }else {
             for (Enchantment enchantment : Enchantment.values()) {
@@ -340,65 +347,20 @@ public class EnchantGUI implements Listener {
         return allPossibleEnchantments;
     }
 
-    private void setAllowedItems() {
-        alloweditems.add(new ItemStack(Material.BOOK));
-        alloweditems.add(new ItemStack(Material.ENCHANTED_BOOK));
-        alloweditems.add(new ItemStack(Material.NETHERITE_SWORD));
-        alloweditems.add(new ItemStack(Material.DIAMOND_SWORD));
-        alloweditems.add(new ItemStack(Material.IRON_SWORD));
-        alloweditems.add(new ItemStack(Material.GOLDEN_SWORD));
-        alloweditems.add(new ItemStack(Material.STONE_SWORD));
-        alloweditems.add(new ItemStack(Material.WOODEN_SWORD));
-        alloweditems.add(new ItemStack(Material.WOODEN_AXE));
-        alloweditems.add(new ItemStack(Material.IRON_AXE));
-        alloweditems.add(new ItemStack(Material.GOLDEN_AXE));
-        alloweditems.add(new ItemStack(Material.DIAMOND_AXE));
-        alloweditems.add(new ItemStack(Material.STONE_AXE));
-        alloweditems.add(new ItemStack(Material.WOODEN_PICKAXE));
-        alloweditems.add(new ItemStack(Material.IRON_PICKAXE));
-        alloweditems.add(new ItemStack(Material.GOLDEN_PICKAXE));
-        alloweditems.add(new ItemStack(Material.DIAMOND_PICKAXE));
-        alloweditems.add(new ItemStack(Material.STONE_PICKAXE));
-        alloweditems.add(new ItemStack(Material.WOODEN_SHOVEL));
-        alloweditems.add(new ItemStack(Material.IRON_SHOVEL));
-        alloweditems.add(new ItemStack(Material.GOLDEN_SHOVEL));
-        alloweditems.add(new ItemStack(Material.DIAMOND_SHOVEL));
-        alloweditems.add(new ItemStack(Material.STONE_SHOVEL));
-        alloweditems.add(new ItemStack(Material.WOODEN_HOE));
-        alloweditems.add(new ItemStack(Material.IRON_HOE));
-        alloweditems.add(new ItemStack(Material.GOLDEN_HOE));
-        alloweditems.add(new ItemStack(Material.DIAMOND_HOE));
-        alloweditems.add(new ItemStack(Material.STONE_HOE));
-        alloweditems.add(new ItemStack(Material.BOW));
-        alloweditems.add(new ItemStack(Material.CROSSBOW));
-        alloweditems.add(new ItemStack(Material.FISHING_ROD));
-        alloweditems.add(new ItemStack(Material.TRIDENT));
-        alloweditems.add(new ItemStack(Material.SHIELD));
-        alloweditems.add(new ItemStack(Material.ELYTRA));
-        alloweditems.add(new ItemStack(Material.LEATHER_HELMET));
-        alloweditems.add(new ItemStack(Material.LEATHER_CHESTPLATE));
-        alloweditems.add(new ItemStack(Material.LEATHER_LEGGINGS));
-        alloweditems.add(new ItemStack(Material.LEATHER_BOOTS));
-        alloweditems.add(new ItemStack(Material.CHAINMAIL_HELMET));
-        alloweditems.add(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
-        alloweditems.add(new ItemStack(Material.CHAINMAIL_LEGGINGS));
-        alloweditems.add(new ItemStack(Material.CHAINMAIL_BOOTS));
-        alloweditems.add(new ItemStack(Material.IRON_HELMET));
-        alloweditems.add(new ItemStack(Material.IRON_CHESTPLATE));
-        alloweditems.add(new ItemStack(Material.IRON_LEGGINGS));
-        alloweditems.add(new ItemStack(Material.IRON_BOOTS));
-        alloweditems.add(new ItemStack(Material.GOLDEN_HELMET));
-        alloweditems.add(new ItemStack(Material.GOLDEN_CHESTPLATE));
-        alloweditems.add(new ItemStack(Material.GOLDEN_LEGGINGS));
-        alloweditems.add(new ItemStack(Material.GOLDEN_BOOTS));
-        alloweditems.add(new ItemStack(Material.DIAMOND_HELMET));
-        alloweditems.add(new ItemStack(Material.DIAMOND_CHESTPLATE));
-        alloweditems.add(new ItemStack(Material.DIAMOND_LEGGINGS));
-        alloweditems.add(new ItemStack(Material.DIAMOND_BOOTS));
-        alloweditems.add(new ItemStack(Material.NETHERITE_HELMET));
-        alloweditems.add(new ItemStack(Material.NETHERITE_CHESTPLATE));
-        alloweditems.add(new ItemStack(Material.NETHERITE_LEGGINGS));
-        alloweditems.add(new ItemStack(Material.NETHERITE_BOOTS));
-        alloweditems.add(new ItemStack(Material.TURTLE_HELMET));
+    private void setAllowedMaterials() {
+        alloweditems.clear();
+        for (Material material : Material.values()) {
+            if (material.isItem()) {
+                ItemStack testItem = new ItemStack(material);
+                for (Enchantment ench : Enchantment.values()) {
+                    if (ench.canEnchantItem(testItem)) {
+                        alloweditems.add(material);
+                        break;
+                    }
+                }
+            }
+        }
+        alloweditems.add(Material.BOOK);
+        alloweditems.add(Material.ENCHANTED_BOOK);
     }
 }
